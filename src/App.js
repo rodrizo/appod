2a287198336c3028d45e1f4e13c29112b25da871
Rodney Rizo <rodneyrizo@outlook.com>
2022-04-20 23:25:11 -0600

    adding routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/">Home</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="favorites" element={<h1>Favs </h1>} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;

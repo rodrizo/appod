9d072bf70ed046cf27b897395a95289053c2b541
Rodney Rizo <rodneyrizo@outlook.com>
2022-04-23 02:16:36 -0600

    adding 'favorites' page

---
+++ src/pages/Favorites.js
@@ -0,0 +1,50 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Text,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+
+export default function Favorites() {
+  const data = [
+    {
+      id: 1,
+      name: "Galaxy M1",
+      date: "19-04-2008",
+    },
+    {
+      id: 2,
+      name: "Milky Way",
+      date: "25-05-1998",
+    },
+  ];
+  return (
+    <div>
+        <Text fontSize='6xl'>Favorites</Text>
+      <br />
+      <br />
+      <TableContainer>
+        <Table variant="striped" colorScheme="blackAlpha">
+          <Thead>
+            <Tr>
+              <Th>Nombre</Th>
+              <Th>Fecha</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((favs) => (
+              <Tr key={favs.id}>
+                <Td>{favs.name}</Td>
+                <Td>{favs.date}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}

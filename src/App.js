1a4940a061ca52d6e1d6b21c2ee3ce1682e945fe
Rodney Rizo <rodneyrizo@outlook.com>
2022-04-23 02:17:38 -0600

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-
+import HomePage from "pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import Navbar from "components/Navbar";
+import Favorites from "pages/Favorites";
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/">Home</Link>
-      </div>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Container maxW='container.xl'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-        <Route path="favorites" element={<h1>Favs </h1>} />
-      </Routes>
-    </Router>
+            <Route path="favorites" element={<Favorites/>} />
+          </Routes>
+        </Container>
+      </Router>
+    </ChakraProvider>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,10 @@
+import { DataGrid } from "components/DataGrid";
 export default function HomePage() {
-    return(
-        <h1>HomePage</h1>
-    )
+  return (
+    <div>
+      <h1>HomePage</h1>
+
+      <DataGrid />
+    </div>
+  );
 }
